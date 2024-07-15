8e03b784a385e5c61c84bf3b2439408159a2598a
Helmut Strasser <helmut.rudolf.strasser@gmail.com>
2024-07-15 13:46:45 +0100

    [TASK] Add void test

---
+++ Tests/Unit/Void/VoidTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Unit\Void;
+
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class VoidTest extends UnitTestCase
+{
+    /**
+     * Test to demonstrate that the TYPO3 Testing Framework
+     * runs 3 assertions in the tearDown method.
+     *
+     * @return void
+     */
+    #[Test]
+    public function void001(): void
+    {
+        self::assertSame(1,1);
+    }
+
+    /**
+     * Another test which ascertains that each test method
+     * calls the setUp and tearDown methods, because after this
+     * method we see 8 assertions in the result.
+     *
+     * @return void
+     */
+    #[Test]
+    public function void002(): void
+    {
+        self::assertSame(1,1);
+    }
+}
