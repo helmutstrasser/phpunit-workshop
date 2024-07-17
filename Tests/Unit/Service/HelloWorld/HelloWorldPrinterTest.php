4ac11d752e3b05cc1161584db2389a1f232110af
Helmut Strasser <helmut.rudolf.strasser@gmail.com>
2024-07-17 16:53:00 +0100

    [TASK] Amend test

--- Tests/Unit/Service/HelloWorld/HelloWorldPrinterTest.php
+++ Tests/Unit/Service/HelloWorld/HelloWorldPrinterTest.php
@@ -1,21 +1,30 @@
 <?php
 declare(strict_types=1);
 
+namespace Unit\Service\HelloWorld;
+
 use PHPUnit\Framework\Attributes\Test;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 use Workshop\Theme\Service\HelloWorld\HelloWorldPrinter;
-use Workshop\Theme\Service\HelloWorld\HelloWorldProvider;
+use Workshop\Theme\Service\HelloWorld\TextProviderInterface;
 
 final class HelloWorldPrinterTest extends UnitTestCase
 {
     /**
+     * Test to demonstrate
+     *
+     * - AAA
+     * - how to mock a dependency
+     * - that either the class or the interface can be mocked
+     *
+     * @return void
      * @throws \PHPUnit\Framework\MockObject\Exception
      */
     #[Test]
     public function printHelloEurope(): void
     {
         $text = "Hello Europe";
-        $helloWorldProviderMock = self::createMock(HelloWorldProvider::class);
+        $helloWorldProviderMock = self::createMock(TextProviderInterface::class);
         $helloWorldProviderMock
             ->expects(self::once())
             ->method('getText')
