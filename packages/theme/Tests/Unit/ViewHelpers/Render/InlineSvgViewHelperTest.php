54cc2a2c174695e0780568139881ae2de3835b2d
Helmut Strasser <h.strasser@supseven.at>
2024-09-25 19:00:12 +0200

    [TASK] Add test case InlineSvgViewHelper

---
+++ packages/theme/Tests/Unit/ViewHelpers/Render/InlineSvgViewHelperTest.php
@@ -0,0 +1,171 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\ViewHelpers\Render;
+
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\Core\Core\ApplicationContext;
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Package\PackageManager;
+use TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContext;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+use Workshop\Theme\ViewHelpers\Render\InlineSvgViewHelper;
+
+/**
+ * InlineSvgViewHelperTest
+ */
+final class InlineSvgViewHelperTest extends UnitTestCase
+{
+    /**
+     * Reset singleton instances
+     *
+     * @var bool $resetSingletonInstances
+     */
+    protected bool $resetSingletonInstances = true;
+
+    /**
+     * @var string $fileName
+     */
+    private string $fileName = 'image.svg';
+
+    /**
+     * @var string $svgContent
+     */
+    private string $svgContent = '<svg></svg>';
+
+    /**
+     * @var string $rootPath
+     */
+    private string $rootPath = 'vfs://root';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $applicationContext = new ApplicationContext('Development');
+        Environment::initialize(
+            $applicationContext,
+            true,
+            true,
+            $this->rootPath,
+            $this->rootPath,
+            '/app/var',
+            '/app/config',
+            '/app',
+            'Linux'
+        );
+
+        $root = vfsStream::setup();
+        $file = vfsStream::newFile($this->fileName)->at($root);
+        $file->setContent($this->svgContent);
+    }
+
+    /**
+     * @return void
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws \TYPO3\CMS\Core\Package\Exception\UnknownPackageException
+     * @throws \TYPO3\CMS\Core\Package\Exception\UnknownPackagePathException
+     * @throws \TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException
+     */
+    #[Test]
+    public function renderStatic(): void
+    {
+        $arguments = ['source' => $this->fileName];
+
+        $renderChildrenClosure = fn () => null;
+        $renderingContext = $this->getTypoScriptFrontendControllerMock();
+
+        $sut = InlineSvgViewHelper::renderStatic($arguments, $renderChildrenClosure, $renderingContext);
+
+        self::assertStringContainsString('<svg role="img" aria-hidden="true"></svg>', $sut);
+    }
+
+    /**
+     * @throws \TYPO3\CMS\Core\Package\Exception\UnknownPackageException
+     * @throws \TYPO3\CMS\Core\Package\Exception\UnknownPackagePathException
+     */
+    #[Test]
+    public function getFilePath(): void
+    {
+        $result = InlineSvgViewHelper::getFilePath($this->fileName);
+
+        self::assertSame($this->rootPath . '/' . $this->fileName, $result);
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws \TYPO3\CMS\Core\Package\Exception\UnknownPackageException
+     * @throws \TYPO3\CMS\Core\Package\Exception\UnknownPackagePathException
+     */
+    #[Test]
+    public function getFilePathWithExt(): void
+    {
+        $packageManager = self::createMock(PackageManager::class);
+        $packageManager->expects(self::once())
+            ->method('resolvePackagePath')
+            ->with('EXT:theme/Resources/Public/Images/' . $this->fileName)
+            ->willReturn('foobar');
+
+        // Prevent the actual class from being instantiated in the sut:
+        GeneralUtility::setSingletonInstance(PackageManager::class, $packageManager);
+
+        $result = InlineSvgViewHelper::getFilePath('EXT:theme/Resources/Public/Images/' . $this->fileName);
+
+        // Assert, condition 'EXT' is true
+        self::assertStringNotContainsString('.svg', $result);
+    }
+
+    /**
+     * @return void
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws \TYPO3\CMS\Core\Package\Exception\UnknownPackageException
+     * @throws \TYPO3\CMS\Core\Package\Exception\UnknownPackagePathException
+     * @throws \TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException
+     */
+    #[Test]
+    public function renderStaticThrowsFileDoesNotExistException(): void
+    {
+        self::expectException(FileDoesNotExistException::class);
+
+        $arguments = [
+            'source' => 'not-existing.svg',
+        ];
+
+        $renderChildrenClosure = fn () => null;
+
+        $renderingContext = $this->getTypoScriptFrontendControllerMock();
+
+        InlineSvgViewHelper::renderStatic($arguments, $renderChildrenClosure, $renderingContext);
+    }
+
+    /**
+     * @return void
+     */
+    #[Test]
+    public function sanitizeId(): void
+    {
+        $sut = InlineSvgViewHelper::sanitizeId('aäöbü*c+#d');
+
+        self::assertSame('a____b___c__d', $sut);
+    }
+
+    /**
+     * @return \TYPO3\CMS\Fluid\Core\Rendering\RenderingContext
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    private function getTypoScriptFrontendControllerMock(): RenderingContext
+    {
+        $typoScriptFrontendController = $this->createMock(TypoScriptFrontendController::class);
+        $request = (new ServerRequest())->withAttribute('frontend.controller', $typoScriptFrontendController);
+        $renderingContext = $this->createMock(RenderingContext::class);
+        $renderingContext->method('getRequest')->willReturn($request);
+
+        return $renderingContext;
+    }
+}
